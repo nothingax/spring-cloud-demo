e2c8222f5aa5bf3f6579770aa82f99dadd7ccde9
zhangjianwei <CSSVDD@gmail.com>
2019-05-20 16:36:41 +0800

    添加gateway

---
+++ spring-cloud-gateway/src/main/java/com/demo/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,37 @@
+package com.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author zhangjianwei
+ */
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+
+    /**
+     * 基本的转发
+     * 当访问http://localhost:8080/baidu
+     * 转发到http://baidu.com
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r ->r.path("/baidu")
+                        .uri("http://baidu.com:80/").id("baidu_route")
+                ).build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
