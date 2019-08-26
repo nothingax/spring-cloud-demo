2760a282c0966dd7b9db367ed15881556d9e7e6a
zhangjianwei <CSSVDD@gmail.com>
2019-08-26 16:21:18 +0800

    统一异常处理

---
+++ spring-cloud-consumer/src/main/java/com/demo/aspect/GlobalExceptionHandler.java
@@ -0,0 +1,147 @@
+package com.demo.aspect;
+
+import com.demo.exception.BizException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Program Name: spring-cloud-demo
+ * <p>
+ * Description: rest controller 统一异常处理
+ *
+ *
+ * <p>
+ *
+ * @author zhangjianwei
+ * @version 1.0
+ * @date 2019/6/20 3:08 PM
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    private boolean includeQueryString = true;
+
+    private boolean includeClientInfo = true;
+
+    private boolean includeHeaders = true;
+
+    private boolean includePayload = true;
+    private int maxPayloadLength = 50;
+
+    /**
+     * 业务异常处理
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BizException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public BizException exceptionHandle(BizException e) {
+        throw e;
+    }
+
+
+    /**
+     * 其他异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Exception exceptionHandle(Exception e, HttpServletRequest request) {
+        // String requestURI = request.g();
+        String message = e.getMessage();
+        System.out.println(message);
+        //
+        // 出现异常的请求url打印出来，参数也可以打印出来
+        // log.error(requestURI);
+        log.error(request.getParameterMap().toString());
+        log.error("系统异常 ====================================", e);
+        // log.error("请求地址={}", request.getRequestURI());
+        // if (request instanceof ContentCachingRequestWrapper) {
+        //     ContentCachingRequestWrapper wrapper = (ContentCachingRequestWrapper) request;
+        //     log.error("请求内容:{}", StringUtils.toEncodedString(wrapper.getContentAsByteArray(), Charset
+        //             .forName(wrapper.getCharacterEncoding())));
+        // }
+        // LeoCaseDeskServiceImpl
+        log.error(getBeforeMessage(request));
+        throw new BizException(e.getMessage(), e.getCause());
+    }
+
+
+    private String getBeforeMessage(HttpServletRequest request) {
+        StringBuilder msg = new StringBuilder();
+        msg.append("uri=").append(request.getRequestURI());
+
+        if (this.includeQueryString) {
+            String queryString = request.getQueryString();
+            if (queryString != null) {
+                msg.append('?').append(queryString);
+            }
+        }
+        if (includeClientInfo) {
+            String client = request.getRemoteAddr();
+            if (StringUtils.hasLength(client)) {
+                msg.append(";client=").append(client);
+            }
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+                msg.append(";session=").append(session.getId());
+            }
+            String user = request.getRemoteUser();
+            if (user != null) {
+                msg.append(";user=").append(user);
+            }
+        }
+
+        if (includeHeaders) {
+            HttpHeaders headers = new ServletServerHttpRequest(request).getHeaders();
+            msg.append(";headers=").append(headers);
+        }
+
+        if (includePayload) {
+            String payload = getMessagePayload(request);
+            if (payload != null) {
+                msg.append(";payload=").append(payload);
+            }
+        }
+
+        return msg.toString();
+    }
+
+
+    private String getMessagePayload(HttpServletRequest request) {
+        ContentCachingRequestWrapper wrapper =
+                WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                int length = Math.min(buf.length, maxPayloadLength);
+                try {
+                    return new String(buf, 0, length, wrapper.getCharacterEncoding());
+                }
+                catch (UnsupportedEncodingException ex) {
+                    return "[unknown]";
+                }
+            }
+        }
+        return null;
+    }
+
+}
